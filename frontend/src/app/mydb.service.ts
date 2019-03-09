f702eef8779593133dd96dc2a32930ce1c95102c
venkatesh <venkateshmorpoju@gmail.com>
2019-03-09 11:55:29 +0530

    asdsad

---
+++ frontend/src/app/mydb.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map' ;
+@Injectable()
+export class MydbService {
+
+  constructor(private http:Http) { }
+
+getData(){
+  return this.http.get("http://localhost:3000/api/mydb")
+  .map(res => res.json());
+}
+addData(info){
+  return this.http.post("http://localhost:3000/api/mydb",info)
+  .map(res => res.json());
+}
+
+}
---
+++ frontend/src/app/show/show.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { MydbService } from '../mydb.service';
+import { Http } from '@angular/http';
+import { Mydb } from '../mydb';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-show',
+  templateUrl: './show.component.html',
+  styleUrls: ['./show.component.css']
+})
+export class ShowComponent implements OnInit {
+
+  constructor(private mydb:MydbService,private http:Http,private _router: Router) { }
+
+   value:boolean=true;
+  model:any=[];
+modell:any=[];
+  display(){
+    this.mydb.getData().subscribe(array=>{
+      this.model=array;
+    })
+
+  }
+  validatedata()
+  {
+
+    for(var i=0;i<this.model.length; i++)
+    {
+      if(this.model[i].emailid === this.modell.emailid)
+      {
+        if(this.model[i].password === this.modell.password)
+        {
+        this.value=false;
+        alert("login happened successfully");
+        
+        this._router.navigate(['/login']);
+        break;
+        }
+      }
+    }
+   if(this.value)
+   {
+     alert("you are not registered member");
+   }
+}
+  ngOnInit() {
+    this.display();
+  }
+
+}
+  
+
+ 
